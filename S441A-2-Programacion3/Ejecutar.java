e7d67cabede08fb3953d114b5bcb23b3c8f32edb
jordirenteriadelacruz-del <jordirenteriadelacruz@gmail.com>
2026-09-01 13:51:03 +0000

    Explicacion del github

---
+++ S441A-2-Programacion3/Ejecutar.java
@@ -0,0 +1,14 @@
+public class Ejecutar {
+    public static void main(String[] args) 
+    
+    {
+        
+        int[] a = {2, 8, 6, 15, 21, 1, 3, 12};
+
+        for(int i = 0; i < a.length; i++){
+            System.out.println("a[" + i + "]=" + a[i]);
+        }
+    }
+    
+}
+
