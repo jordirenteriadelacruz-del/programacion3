5d6ecd895da12c10086fcd6a257e78376bdca98e
jordirenteriadelacruz-del <jordirenteriadelacruz@gmail.com>
2026-09-01 14:33:51 +0000

    Llenar un arreglo de manera aleatoria con numeros de el 1 al 100 y mostrarlos

--- S441A-2-Programacion3/Ejecutar.java
+++ S441A-2-Programacion3/Ejecutar.java
@@ -1,14 +1,31 @@
+import java.util.Random;
+
 public class Ejecutar {
     public static void main(String[] args) 
     
     {
-        
+        // Forma 1 - Creacion del arreglo
         int[] a = {2, 8, 6, 15, 21, 1, 3, 12};
-
+        //recorremos el arreglo
         for(int i = 0; i < a.length; i++){
             System.out.println("a[" + i + "]=" + a[i]);
         }
-    }
-    
-}
+
+        //Forma 2 -  llenando el arreglo de manera aleatoria
+        int[] b = new int[10];
+        
+       
+        int max = 100;
+        int min = 1;
+
+        for(int i = 0; i<b.length; i++){
+           b[i] = (int)(Math.random() *  (max - min + 1)) - min;
+   }
+   //mostrar el arreglo
+   for(int i = 0; i < b.length; i++){
+            System.out.println("b[" + i + "]=" + b[i]);
+        }
+
+  }     
+ }
 
