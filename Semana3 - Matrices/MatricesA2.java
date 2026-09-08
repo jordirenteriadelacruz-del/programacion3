32813559c7c0d20ebd6308060ff9d3711f4ced7d
jordirenteriadelacruz-del <jordirenteriadelacruz@gmail.com>
2026-09-08 13:50:54 +0000

    Matrices

--- Semana3 - Matrices/MatricesA2.java
+++ Semana3 - Matrices/MatricesA2.java
@@ -1,5 +1,17 @@
 public class MatricesA2 {
-public static void main(String[] args) {
-    System.out.println(x: "Matrices");
-}    
+    public static void main(String[] args) {
+        
+        //Declaracion de una matriz 3x3 (forma 1)
+        int[][] m = {{5, 8, 6},
+                     {1, 3, 2},  
+                     {4, 9, 7}};  
+        //Recorrer la matriz
+        for (int i = 0; i <m.length; i++){
+        for (int j = 0; i < m[0].length; j++){
+            System.out.println("m["+i+"]["+j+"]= "+m[i][j]); 
+        }
+    }
+
+
+    }
 }
