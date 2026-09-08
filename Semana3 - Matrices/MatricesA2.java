d7185ec676ecb4037d80defb444b6c9260a917b0
jordirenteriadelacruz-del <jordirenteriadelacruz@gmail.com>
2026-09-08 14:14:07 +0000

    Decorar mejor la matriz

--- Semana3 - Matrices/MatricesA2.java
+++ Semana3 - Matrices/MatricesA2.java
@@ -1,17 +1,32 @@
 public class MatricesA2 {
     public static void main(String[] args) {
-        
-        //Declaracion de una matriz 3x3 (forma 1)
+       
+        //Declaración de una matriz 3X3 (Forma 1)
         int[][] m = {{5, 8, 6},
-                     {1, 3, 2},  
-                     {4, 9, 7}};  
+                     {1, 3, 2},
+                     {4, 9, 7}};
+       
         //Recorrer la matriz
-        for (int i = 0; i <m.length; i++){
-        for (int j = 0; i < m[0].length; j++){
-            System.out.println("m["+i+"]["+j+"]= "+m[i][j]); 
+        for (int i = 0; i < m.length; i++) { //m.length -> siempre será para recorrer las filas
+            for (int j = 0; j < m[0].length; j++) {   //m[0].length -> siempre será para recorrer las columnas
+                System.out.println("m["+i+"]["+j+"]="+m[i][j]);        
+            }
         }
-    }
-
-
+ 
+        //Visualizar de mejor manera la matriz
+        String cad = "";
+        for (int i = 0; i < m.length; i++) { //m.length -> siempre será para recorrer las filas
+            for (int j = 0; j < m[0].length; j++) {   //m[0].length -> siempre será para recorrer las columnas
+                cad += "|"+m[i][j];        
+            }
+            cad+="| \n";
+        }
+ 
+        System.out.println(cad);
+ 
+ 
+ 
     }
 }
+
+
