d58aa2baf799b0d3a865382cb00f17b94cfbd86d
jordirenteriadelacruz-del <jordirenteriadelacruz@gmail.com>
2026-09-14 17:29:35 +0000

    ejercicio

---
+++ TallerMatrices/Ejernum21.java
@@ -0,0 +1,21 @@
+package TallerMatrices;
+
+public class Ejernum21 {
+    
+    public static void main(String[] args) {
+        
+        int m = 3;
+        int m = 3;
+      
+        int[][] matriz = new int[m][m];
+
+        for(int i = 0; i < m; i++){
+        
+        
+        }
+
+
+        
+
+    }
+}
